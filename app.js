0a3bdbaef4bc6ed85271fc51143cb6b6b19002fb
Henry Thiemann <heenryt@gmail.com>
2014-02-11 19:06:36 -0800

    added mongodb

--- app.js
+++ app.js
@@ -42,6 +42,20 @@ app.get('/listen', listen.render);
 app.get('/inbox', inbox.render);
 app.get('/user/:id(\\d+)', user.show);
 
+//mongodb
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
