82f296d5f17f7f364fa3068f651b16ec2b2c58e5
Henry Thiemann <heenryt@gmail.com>
2014-02-14 00:18:54 -0800

    heroku

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ var mongo = require('mongodb');
 var monk = require('monk');
 
 // for local testing
-var db = monk('localhost:27017/songshare'); 
+// var db = monk('localhost:27017/songshare'); 
 // for heroku
-// var db = monk(process.env.MONGOLAB_URI);
+var db = monk(process.env.MONGOLAB_URI);
 
 app.get('/', routes.index);
 app.get('/userlist', routes.userlist(db));
