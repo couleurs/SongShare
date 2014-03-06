e0c48fb1932a21262d663dd23d4286302e1b2fdc
Henry Thiemann <heenryt@gmail.com>
2014-03-06 09:01:44 -0800

    heroku changes

--- public/js/videoplayer.js
+++ public/js/videoplayer.js
@@ -59,9 +59,9 @@ $( document ).ready(function() {
 	username = $('#chatsubmit').data('username');
 
 	//local
-	socket = io.connect('http://localhost/listen'+listeningroom_id);
+	// socket = io.connect('http://localhost/listen'+listeningroom_id);
 	//heroku	
-	// socket = io.connect('https://songshare147.herokuapp.com/listen'+listeningroom_id);
+	socket = io.connect('https://songshare147.herokuapp.com/listen'+listeningroom_id);
 	setupSocket();
 
 	if (secondConnection && onIframeReady)
