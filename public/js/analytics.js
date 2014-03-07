3e503c175f1abbba6543bbb0c7d935cc9b34ac39
Nathan Lee <leeso@stanford.edu>
2014-03-06 16:04:39 -0800

    analytics

---
+++ public/js/analytics.js
@@ -0,0 +1,27 @@
+'use strict';
+
+$('.glyphicon.glyphicon-chevron-left').click(backClicked);
+$('.btn.btn-warning.dashboard-share-song').click(sharesongClicked);
+$('.btn.btn-sm.btn-danger').click(rmfriendClicked);
+$('.btn btn-sm btn-info').click(pickedSong);
+$('.glyphicon.glyphicon-home').click(returnedHome);
+
+function backClicked(event) {
+	ga("send", "event", "back", "click");
+}
+
+function sharesongClicked(event) {
+	ga("send", "event", "share", "click");
+}
+
+function rmfriendClicked(event) {
+	ga("send", "event", "removefriend", "click");
+}
+
+function pickedSong(event) {
+	ga("send", "event", "pick song", "click");
+}
+
+function returnedHome(event) {
+	ga("send", "event", "return home", "click");
+}
